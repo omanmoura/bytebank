6274784a0ed781ec2040f5c93956857ecf19e55e
Oman <omanmoura@gmail.com>
2022-03-04 16:52:14 -0300

    criando funções saque e deposito na classe contacorrente

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 class Cliente {
     nome;
     cpf;
+}
+
+class ContaCorrente {
     agencia;
     saldo;
+
+    depositar(valor) {
+        if (valor >= 0) {
+            this.saldo += valor;
+        } else {
+            console.log ("Valor inválido.");
+        }
+    }
+
+    sacar(valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+        } else {
+            console.log("Saldo insuficiente.");
+        }
+    }
 }
 
 const cliente1 = new Cliente();
 cliente1.nome = "Oman";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
 const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 22233344409;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
 console.log(cliente1);
 console.log(cliente2);
