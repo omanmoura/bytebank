40712777920f3c0ac5a280a404451503ac16e6c2
Oman <omanmoura@gmail.com>
2022-03-07 16:59:01 -0300

    adicionando atributos estáticos

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,9 +1,32 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente {
+    static numeroDeContas = 0;
     agencia;
-    cliente;
+    _cliente;
+
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente() {
+        return this._cliente;
+    }
 
     //#saldo = 0; <---- CASO FOSSE UTILIZADO O NODE.JS, CONSEGUIRIA INSERIR ATRIBUTO PRIVADO À CLASSE
     _saldo = 0; // <---- CONVENÇÃO PARA ATRIBUTO "PRIVADO"
+
+    get saldo() {
+        return this._saldo;
+    }
+
+    constructor (agencia, cliente) {
+        this.agencia = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numeroDeContas++;
+    }
 
     depositar(valor) {
         if (valor >= 0) {
@@ -28,6 +51,6 @@ export class ContaCorrente {
     transferir(valor, conta) {
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
-        console.log(`O valor transferido para a conta do(a) ${conta.cliente.nome} foi R$${valorSacado}.`)
+        console.log(`O valor transferido para a conta do(a) ${conta._cliente.nome} foi R$${valorSacado}.`)
     }
 }
