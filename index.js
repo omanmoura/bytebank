1aa581f601674e9dac402711baa2c825beeb55d4
Oman <omanmoura@gmail.com>
2022-03-07 16:57:33 -0300

    adicionando atributos estáticos

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
 import {Cliente} from "./Cliente.js";
 import {ContaCorrente} from "./ContaCorrente.js";
 
-const cliente1 = new Cliente();
-cliente1.nome = "Oman";
-cliente1.cpf = 11122233309;
+const cliente1 = new Cliente("Oman", 11122233309);
+const cliente2 = new Cliente("Alice", 22233344409);
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 22233344409;
+const conta1 = new ContaCorrente(1001, cliente1);
+const conta2 = new ContaCorrente(1002, cliente2);
 
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
-contaCorrenteRicardo.cliente = cliente1;
-
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
-conta2.agencia = 1002;
-
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.sacar(205);
-console.log(contaCorrenteRicardo);
+conta1.depositar(100);
+conta1.sacar(205);
+console.log(conta1);
 console.log(conta2);
 
-contaCorrenteRicardo.transferir(50,conta2);
+conta1.transferir(50,conta2);
 
-console.log(conta2.saldo);
+conta1.cliente.nome = "André";
+console.log(conta1);
+
+console.log(ContaCorrente.numeroDeContas);
