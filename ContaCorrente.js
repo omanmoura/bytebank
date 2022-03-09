f5e8c6781f0b57ea73b3817ca1fbc1e2674f7a54
Oman <omanmoura@gmail.com>
2022-03-09 17:23:35 -0300

    Adicionando algumas regras de negócio

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,4 +1,3 @@
-import { Cliente } from "./Cliente.js";
 import { Conta } from "./Conta.js";
 
 export class ContaCorrente extends Conta {
@@ -7,5 +6,20 @@ export class ContaCorrente extends Conta {
     constructor (cliente, agencia) {
         super(0, cliente, agencia);
         ContaCorrente.numeroDeContas++;
+    }
+
+    sacar (valor) {
+        let taxa = valor*0.1;
+        const valorTotalSacado = valor + taxa;
+
+        if (valorTotalSacado <= this._saldo) {
+            this._saldo -= valorTotalSacado;
+            console.log(`\nValor sacado: R$${valor}`);
+            console.log(`Taxa de saque: R$${taxa}`)
+            console.log(`Saldo atual: R$${this._saldo}\n`);
+        } else {
+            console.log(`\nValor indisponível para saque.`);
+            console.log(`Saldo atual: R$${this._saldo}`);
+        }
     }
 }
--- index.js
+++ index.js
@@ -6,9 +6,8 @@ const cliente1 = new Cliente("Oman", 11122233309);
 const conta1 = new ContaCorrente(cliente1, 1001);
 
 conta1.depositar(500);
-conta1.sacar(100);
+conta1.sacar(10);
+console.log(conta1);
 
 const contaPoupanca = new ContaPoupanca(50, cliente1, conta1._agencia);
 
-console.log(conta1);
-console.log(contaPoupanca);
